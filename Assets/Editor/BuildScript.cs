ace4273161e7dfe6e3c4f6e4d80f3e191ea8ffe1
Son Nguyen Hoang <fh8fo3j30xe6gl0ax3h64vhvyzk7v6@bots.bitbucket.org>
2025-10-23 11:46:20 +0700

    + build

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -1,8 +1,10 @@
 using UnityEditor;
 using System.IO;
+using UnityEngine;
 
 public static class BuildScript
 {
+    [MenuItem("Tool/Build")]
     public static void BuildAndroid()
     {
         string buildPath = "Builds/Android/MyGame.apk";
@@ -10,7 +12,7 @@ public static class BuildScript
             Directory.CreateDirectory("Builds/Android");
 
         BuildPipeline.BuildPlayer(
-            new[] { "Assets/Scenes/MainScene.unity" }, // change scenes as needed
+            new[] { "Assets/Scenes/SampleScene.unity" }, // change scenes as needed
             buildPath,
             BuildTarget.Android,
             BuildOptions.None
